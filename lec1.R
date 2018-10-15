25bed7b22518599dee341d7a703b06f83e978a73
Daguila27 <daguila.gallardo@gmail.com>
2018-10-14 22:25:33 -0300

    pregunta 3 y 4 wswwswO0n branch master

--- lec1.R
+++ lec1.R
@@ -21,6 +21,6 @@ for(i in 2:18){
 }
 print(mayor)
 ggplot(data, aes(x=log(data$likes), y=log(data$views)))+geom_point(aes(color = factor(data$category),shape = factor(data$dislikes>data$likes)))+stat_smooth(method = "glm",col = "#C42128",se = FALSE,size = 1)+labs(title = "Laboratorio de ESTACA 1",x="Likes",y="Vistas")
-ggplot(data, aes(x=log(data$views), y=log(data$likes/(data$dislikes + 1))))+geom_point(aes(shape=factor(data$comment_count>1000000 %in% c(TRUE) ) , color = factor(data$category)))+stat_smooth(geom = "smooth", method = "glm",col = "#C42128",se = FALSE,size = 1)+labs(title = "Laboratorio de ESTACA 1",x="Likes",y="Ratio")
+ggplot(data, aes(x=log(data$views), y=log(data$likes/(data$dislikes + 1))))+geom_point(aes(shape=factor(data$comment_count>1000 %in% c(FALSE) ) , color = factor(data$category)))+stat_smooth(geom = "smooth", method = "glm",col = "#C42128",se = FALSE,size = 1)+labs(title = "Laboratorio de ESTACA 1",x="Likes",y="Ratio")
 
 
